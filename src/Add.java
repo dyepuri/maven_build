2daa1ac5fa24d9cd634f79c5d96a485b8bf8928c
dyepuri <yepuridivya@gmail.com>
2018-02-21 14:32:50 -0600

    commiting the pom changes

--- src/Add.java
+++ src/Add.java
@@ -2,11 +2,11 @@
 public class Add {
 
 	public static void main(String[] args) {
-		int a=10;
-		int b=15;
-		int c= a+b;
-		System.out.println("value of c is  "+c);
+				int a=10;
+				int b=15;
+				int c= a+b;
+				System.out.println("value of c is  "+c);
 
-	}
+			}
 
-}
+		}
